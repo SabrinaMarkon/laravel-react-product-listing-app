9678f15659660d343440e46591e069fe9906efac
Sabrina Markon <phpsitescripts@outlook.com>
2018-03-05 23:55:04 -0700

    Created Main.js React component in assets

---
+++ resources/assets/js/components/Main.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+/* Our Main Component */
+class Main extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            products: [],
+        }
+    }
+    /* After the component renders, call the lifecycle method ComponentDidMount. */
+    componentDidMount() {
+        /* JS fetch() API promise to get the data */
+        fetch('/products')
+            .then(response => {
+                return response.json();
+            })
+            .then(products => {
+                /* store fetched product in state. */
+                this.setState({ products });
+            });
+    }
+    /* */
+    renderProducts() {
+        return this.state.products.map(product => {
+            return (
+                /* if we use a list we need a key attribute that is unique for each li */
+                <li key={ product.id } >
+                    { product.title }
+                </li>
+            );
+        });
+    }
+    render() {
+        return(
+            <div>
+                <ul>
+                    { this.renderProducts() }
+                </ul>
+            </div>
+        );
+    }
+}
